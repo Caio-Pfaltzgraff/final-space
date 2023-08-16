913bfcd3c17f967f4c408d4b01ee21140f767282
Caio-Pfaltzgraff <caio.rllcr@gmail.com>
2023-08-16 17:15:20 -0300

    adicionando o banner e configuracoes

--- src/pages/PaginaInicial/index.jsx
+++ src/pages/PaginaInicial/index.jsx
@@ -1,7 +1,18 @@
+import Banner from "../../components/Banner";
 import Cabecalho from "../../components/Cabecalho";
+import Menu from "../../components/Menu";
+import styles from "./PaginaInicial.module.scss"
 
 export default function PaginaInicial() {
     return (
-        <Cabecalho />
+        <>
+            <Cabecalho />
+            <main>
+                <section className={styles.principal}>
+                    <Menu />
+                    <Banner />
+                </section>
+            </main>
+        </>
     )
 }
