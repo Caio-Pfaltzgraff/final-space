2e65358658a0fa14a951af2e9377c36d9e0fcc07
Caio-Pfaltzgraff <caio.rllcr@gmail.com>
2023-08-17 14:18:06 -0300

    adicionando filtro por tags

--- src/components/Galeria/Cards/index.jsx
+++ src/components/Galeria/Cards/index.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import Card from './Card'
 
-export default function Cards({fotos, styles}) {
+export default function Cards({itens, styles}) {
   return (
     <ul className={styles.galeria__cards}>
-          {fotos.map((foto) => {
+          {itens.map((foto) => {
             return (
               <Card key={foto.id} foto={foto} styles={styles}/>
             )
